229a9f33c6e8bc2b770bf345eb2097177eaeb6aa
Vo Hai <vohai10112005@gmail.com>
2025-09-26 00:40:39 +0700

    fix: add some redirection for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import path from 'path';
+
 import { connectDB } from './lib/db.js';
 
 import authRoutes from './routes/auth.route.js';
@@ -14,6 +16,7 @@ import { app, server}  from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 // Increase the body size limit to handle larger image uploads
 app.use(express.json({ limit: '10mb' }));
@@ -28,6 +31,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/friends", friendRoutes);
 app.use("/api/users", userRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 server.listen(PORT, () => {
     console.log('Server is running on PORT:' + PORT);
